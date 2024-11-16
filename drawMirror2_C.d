6b381b763ee7b8ea32b4c1acde565ad8d0415c1c
darp256 <181456259+darp256@users.noreply.github.com>
2024-11-16 11:32:10 +0200

    Add files via upload

---
+++ drawMirror2_C.d
@@ -0,0 +1,8 @@
+
+# DO NOT DELETE
+
+./drawMirror2_C.so: /usr/include/root/TCanvas.h /usr/include/root/TH3D.h
+./drawMirror2_C.so: /usr/include/root/TProfile2D.h
+./drawMirror2_C.so: /usr/include/root/TString.h /usr/include/root/TStyle.h
+./drawMirror2_C.so: /usr/include/root/RVersion.h /usr/include/root/ROOT/RConfig.hxx /usr/include/root/TClass.h /usr/include/root/TDictAttributeMap.h /usr/include/root/TInterpreter.h /usr/include/root/TROOT.h /usr/include/root/TBuffer.h /usr/include/root/TMemberInspector.h /usr/include/root/TError.h /usr/include/root/RtypesImp.h /usr/include/root/TIsAProxy.h /usr/include/root/TFileMergeInfo.h /usr/include/root/TCollectionProxyInfo.h /usr/bin/rootcling
+drawMirror2_C__ROOTBUILDVERSION= 6.32.06
---
+++ drawMirror_C.d
@@ -0,0 +1,7 @@
+
+# DO NOT DELETE
+
+./drawMirror_C.so: /usr/include/root/TCanvas.h /usr/include/root/TH3D.h
+./drawMirror_C.so: /usr/include/root/TString.h
+./drawMirror_C.so: /usr/include/root/RVersion.h /usr/include/root/ROOT/RConfig.hxx /usr/include/root/TClass.h /usr/include/root/TDictAttributeMap.h /usr/include/root/TInterpreter.h /usr/include/root/TROOT.h /usr/include/root/TBuffer.h /usr/include/root/TMemberInspector.h /usr/include/root/TError.h /usr/include/root/RtypesImp.h /usr/include/root/TIsAProxy.h /usr/include/root/TFileMergeInfo.h /usr/include/root/TCollectionProxyInfo.h /usr/bin/rootcling
+drawMirror_C__ROOTBUILDVERSION= 6.32.06
